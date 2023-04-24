3a693b4b9d6d4e9af42d7e612b4a4ce142b5f7b6
KMN <kuwwatt@gmail.com>
2023-04-24 18:38:40 +0200

    HrGetRequest.java

--- src/test/java/com/cydeo/Day2/HrGetRequest.java
+++ src/test/java/com/cydeo/Day2/HrGetRequest.java
@@ -1,7 +1,15 @@
 package com.cydeo.Day2;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HrGetRequest {
 
@@ -9,6 +17,44 @@ public class HrGetRequest {
     //BeforeAll is a annotation equals to @BeforeClass in TestNG, we use with static method name
     @BeforeAll
     public static void init(){
-        RestAssured.baseURI="http://54.226.109.52:1000/ords/hr";
+        //save baseUrl inside this variable, so we don't need to type each http method
+        baseURI = "http://54.226.109.52:1000/ords/hr";
+    }
+
+
+    @DisplayName("GET request to /regions")
+    @Test
+    public void test1(){
+        Response response = get("/regions");
+
+        //print the status code
+        System.out.println("response.statusCode() = " + response.statusCode());
+    }
+
+
+    @DisplayName("GET request to /regions/2")
+    @Test
+    public void test2(){
+        //Given: Accept type application/json
+        //When user send Get request to regions/2
+        Response response = given().accept(ContentType.JSON)
+                .when()
+                .get("/regions/2");
+
+        //Then status code must 200
+        System.out.println("response.statusCode() = " + response.statusCode());
+        //verify status code
+        assertEquals(200,response.statusCode());
+
+        //And contentType equals to application/json
+        System.out.println("response.contentType() = " + response.contentType());
+        //verify ContentType
+        assertEquals("application/json",response.contentType());
+
+        //print full body
+        response.prettyPrint();
+
+        //And response body contains Americas
+        assertTrue(response.getBody().asString().contains("Americas"));
     }
 }
