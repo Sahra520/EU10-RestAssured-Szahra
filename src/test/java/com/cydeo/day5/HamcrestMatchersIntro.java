f70c2d4d8a5de1d635686bbbb5f760a5fc6ab7f3
KMN <kuwwatt@gmail.com>
2023-04-29 22:03:46 +0200

    HamcrestMatchersIntro.java

--- src/test/java/com/cydeo/day5/HamcrestMatchersIntro.java
+++ src/test/java/com/cydeo/day5/HamcrestMatchersIntro.java
@@ -1,11 +1,17 @@
 package com.cydeo.day5;
 
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 public class HamcrestMatchersIntro {
 
     @Test
     public void simpleTest1(){
+
+        assertThat(5+5, is(10));
 
     }
 }
