b76374042ebae374851dcb0ccdfa30a46ce1026a
KMN <kuwwatt@gmail.com>
2023-05-06 22:56:13 +0200

    fghn.java

---
+++ src/test/java/com/cydeo/reviewSaturdayClasses/fghn.java
@@ -0,0 +1,32 @@
+package com.cydeo.reviewSaturdayClasses;
+
+public class fghn {
+//    pm.test("Status code is 200", function () {
+//        pm.response.to.have.status(200);
+//    });
+//
+//pm.test("Content-Type is present", function () {
+//        pm.response.to.have.header("Content-Type");
+//    });
+//
+//pm.test("Transfer-Encoding is present", function () {
+//        pm.response.to.have.header("Transfer-Encoding");
+//    });
+//
+//pm.test("Connection is present", function () {
+//        pm.response.to.have.header("connection");
+//    });
+//
+//pm.test("Keep alive value should be timeout=60", function () {
+//        pm.response.to.be.header("Keep-Alive","timeout=60");
+//    });
+//
+//pm.test("Connection value should be keep alive", function () {
+//        pm.response.to.be.header("Connection","keep-alive");
+//    });
+//
+//
+//pm.test("Response time is less than 500ms", function () {
+//    pm.expect(pm.response.responseTime).to.be.below(500);
+//    });
+}
