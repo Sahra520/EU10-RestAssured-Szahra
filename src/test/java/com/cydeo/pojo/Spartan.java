2350221c8a39c704d3441b1f8c4ac8f25948c936
KMN <kuwwatt@gmail.com>
2023-05-13 22:18:24 +0200

    Spartan.java

--- src/test/java/com/cydeo/pojo/Spartan.java
+++ src/test/java/com/cydeo/pojo/Spartan.java
@@ -1,5 +1,12 @@
 package com.cydeo.pojo;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Spartan {
  /*
    "id": 15,
@@ -18,46 +25,5 @@ public class Spartan {
     //getter() and setter() methods
     //toString() method
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public long getPhone() {
-        return phone;
-    }
-
-    public void setPhone(long phone) {
-        this.phone = phone;
-    }
-
-
-    @Override
-    public String toString() {
-        return "Spartan{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", gender='" + gender + '\'' +
-                ", phone=" + phone +
-                '}';
-    }
 }
