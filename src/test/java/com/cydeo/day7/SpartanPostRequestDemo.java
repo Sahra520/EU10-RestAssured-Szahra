681f8c94586bd38b311aa077c4602c17e6b65210
KMN <kuwwatt@gmail.com>
2023-05-13 22:17:14 +0200

    SpartanPostRequestDemo.java

--- src/test/java/com/cydeo/day7/SpartanPostRequestDemo.java
+++ src/test/java/com/cydeo/day7/SpartanPostRequestDemo.java
@@ -3,8 +3,12 @@ package com.cydeo.day7;
 import com.cydeo.utilities.SpartanTestBase;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -40,8 +44,40 @@ and same data what is posted
                 "\"phone\": 8877445596}";
 
         Response response = given().accept(ContentType.JSON).and()
-                .contentType(ContentType.JSON)
+                //what we are asking from api which is JSON response
+                .contentType(ContentType.JSON)//what we are sending to api, which is JSON also
                 .body(requestJsonBody)
+                .when()
+                //.log().all()
+                .post("/api/spartans");
+
+        //verify status code
+        assertThat(response.statusCode(),is(201));
+        assertThat(response.contentType(), is("application/json"));
+
+        String expectedResponseMessage = "A Spartan is Born!";
+        assertThat(response.path("success"),is(expectedResponseMessage));
+
+        assertThat(response.path("data.name"), is("Severus"));
+        assertThat(response.path("data.gender"),is("Male"));
+        assertThat(response.path("data.phone"),is(8877445596L));
+    }
+
+
+
+    @DisplayName("POST with Map to JSON")
+    @Test
+    public void postMethod2(){
+        //create a Map to keep Json body information
+        Map<String,Object> requestJsonMap = new LinkedHashMap<>();
+        requestJsonMap.put("gender", "Male");
+        requestJsonMap.put("name", "Severus");
+        requestJsonMap.put("phone", 8877445596L);
+
+        Response response = given().accept(ContentType.JSON).and()
+                //what we are asking from api which is JSON response
+                .contentType(ContentType.JSON)//what we are sending to api, which is JSON also
+                .body(requestJsonMap).log().all()
                 .when()
                 .post("/api/spartans");
 
@@ -51,5 +87,12 @@ and same data what is posted
 
         String expectedResponseMessage = "A Spartan is Born!";
         assertThat(response.path("success"),is(expectedResponseMessage));
+
+        assertThat(response.path("data.name"), is("Severus"));
+        assertThat(response.path("data.gender"),is("Male"));
+        assertThat(response.path("data.phone"),is(8877445596L));
+
+        response.prettyPrint();
+
     }
 }
